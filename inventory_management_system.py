210b21702f6c4b645e19d90c5c1360f1fa7f3fab
Sadaf Heidari <sadafheidari74@gmail.com>
2026-08-07 12:26:22 -0400

    Add product creation and inventory viewing features

--- inventory_management_system.py
+++ inventory_management_system.py
@@ -15,13 +15,54 @@ def display_menu():
 def get_user_choice():
     return input("Please select an option from the menu: ").strip()
 
+def add_product():
+    name =input(" please enter your product name: ").strip()
+    code =input("please enter the product code reference: ").strip()
+    category =input("please enter the product category: " ).strip()
+    weight =input("please enter the product weight: ").strip()
+    quantity =int(input("please enter the product available quantity: "))
+    material =input("please enter the product material: ").strip()
+    origin =input("please enter the product origin: ").strip()
+    price =float(input("please enter the product price: "))
+
+    product= {"name":name,
+              "code":code,
+              "category":category,
+              "weight":weight,
+              "quantity":quantity,
+              "material":material,
+              "origin":origin,
+              "price":price
+              }
+    inventory.append(product)
+    print("\nProduct added successfully.")
+
+def view_inventory():
+    if inventory :
+        print("\nInventory:")
+        for product in inventory:
+            print(f"name: {product['name']},"
+                  f"code: {product['code']},"
+                  f"category: {product['category']},"
+                  f"weight: {product['weight']},"
+                  f"quantity: {product['quantity']},"
+                  f"material: {product['material']},"
+                  f"origin: {product['origin']},"
+                  f"price: {product['price']}")
+
+    else:
+        print("\nInventory is empty.")
+
 
 def main():
     while True:
         display_menu()
         user_choice = get_user_choice()
-
-        if user_choice == "6":
+        if user_choice == "1":
+            add_product()
+        elif user_choice == "2":
+            view_inventory()
+        elif user_choice == "6":
             print("Thank you for using Inventory Management System.")
             break
         else:
